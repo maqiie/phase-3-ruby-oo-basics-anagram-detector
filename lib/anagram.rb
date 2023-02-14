44e677a90ff446d7dd6bda7b5c9d1e37ebf2ba53
maqiie <99504010+maqiie@users.noreply.github.com>
2023-02-14 15:35:41 +0300

    completed the lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(candidates)
+    # implementation here
+  end
+end
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(candidates)
+    candidates.select { |candidate| anagram?(candidate) }
+  end
+
+  private
+
+  def anagram?(candidate)
+    candidate.downcase.chars.sort == word.downcase.chars.sort && candidate.downcase != word.downcase
+  end
+end
